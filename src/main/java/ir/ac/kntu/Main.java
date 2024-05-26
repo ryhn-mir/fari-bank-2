f4dc6d5cc1fec3202e13c33a2fb63be6d1a50702
Reyhane <Rmirarabshahi2005@gmail.com>
2024-05-26 10:30:46 +0330

    fix : close scanner

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -1,11 +1,13 @@
 package ir.ac.kntu;
 
 import ir.ac.kntu.faribank.FariBank;
+import ir.ac.kntu.util.ScannerWrapper;
 
 public class Main {
 
     public static void main(String[] args) {
         FariBank.start();
+        ScannerWrapper.getInstance().close();
     }
 
 }
