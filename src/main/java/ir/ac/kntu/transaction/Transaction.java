c5e68959f24a844f27364903a8358ca9277d3da3
Reyhane <Rmirarabshahi2005@gmail.com>
2024-06-15 18:22:48 +0330

    chore : change toString inn transaction class

--- src/main/java/ir/ac/kntu/transaction/Transaction.java
+++ src/main/java/ir/ac/kntu/transaction/Transaction.java
@@ -16,6 +16,7 @@ public class Transaction {
     private TransactionKind transactionKind;
 
 
+
     public Transaction(String receiverFirstName, String receiverLastName, String receiverNo, String senderNo, TransactionKind transactionKind) {
         this.receiverFirstName = receiverFirstName;
         this.receiverLastName = receiverLastName;
