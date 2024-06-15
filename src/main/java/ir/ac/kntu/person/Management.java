9d4636b3dbf067cb5330af12f593794616f027f2
Reyhane <Rmirarabshahi2005@gmail.com>
2024-06-15 18:08:48 +0330

    chore : add userState field in management class

--- src/main/java/ir/ac/kntu/person/Management.java
+++ src/main/java/ir/ac/kntu/person/Management.java
@@ -2,10 +2,21 @@ package ir.ac.kntu.person;
 
 public class Management extends Person {
     private String userName;
-
+    private UserState userState;
+    private Permission permission;
     public Management(String firstName, String lastName, String password, String userName) {
         super(firstName, lastName, password);
         this.userName = userName;
+        this.userState = UserState.UNBLOCKED;
+        permission = new Permission();
+    }
+
+    public UserState getUserState() {
+        return userState;
+    }
+
+    public void setUserState(UserState userState) {
+        this.userState = userState;
     }
 
     public String getUserName() {
@@ -14,5 +25,20 @@ public class Management extends Person {
 
     public void setUserName(String userName) {
         this.userName = userName;
+    }
+
+    public Permission getPermission() {
+        return permission;
+    }
+
+    public void setPermission(Permission permission) {
+        this.permission = permission;
+    }
+
+    @Override
+    public String toString() {
+        return "Management{" + super.toString() +
+                "userName='" + userName + '\'' +
+                '}';
     }
 }
