a0f9f191432e02c5b99531b554e5e91be9259572
Reyhane <Rmirarabshahi2005@gmail.com>
2024-06-15 18:00:43 +0330

    feat : add fundKind

---
+++ src/main/java/ir/ac/kntu/fund/FundKind.java
@@ -0,0 +1,15 @@
+package ir.ac.kntu.fund;
+
+public enum FundKind {
+    SAVING("saving"),
+    REMAINING("remaining"),
+    PROFIT("profit");
+    private final String kind;
+    private FundKind(String kind) {
+        this.kind = kind;
+    }
+
+    public String getKind() {
+        return kind;
+    }
+}
