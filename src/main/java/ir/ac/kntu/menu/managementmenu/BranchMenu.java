579d984b20e76cf647d310f9c122d5a974b143b1
Reyhane <Rmirarabshahi2005@gmail.com>
2024-06-15 19:11:12 +0330

    chore : allocate admin

--- src/main/java/ir/ac/kntu/menu/managementmenu/BranchMenu.java
+++ src/main/java/ir/ac/kntu/menu/managementmenu/BranchMenu.java
@@ -2,6 +2,8 @@ package ir.ac.kntu.menu.managementmenu;
 
 import ir.ac.kntu.Constant;
 import ir.ac.kntu.database.AnswerRequestDatabase;
+import ir.ac.kntu.person.Management;
+import ir.ac.kntu.person.Permission;
 import ir.ac.kntu.request.Request;
 import ir.ac.kntu.request.RequestOption;
 import ir.ac.kntu.request.RequestState;
@@ -9,23 +11,23 @@ import ir.ac.kntu.menu.MainMenu;
 
 public class BranchMenu extends MainMenu {
 
-    public void branchMenu(AnswerRequestDatabase answerDB) {
+    public void branchMenu(AnswerRequestDatabase answerDB, Management management) {
         int number = 0;
         while (number != 99) {
             RequestOption.print();
             number = getNumber();
             switch (number) {
                 case 1:
-                    contact(answerDB);
+                    checkContact(management, answerDB);
                     break;
                 case 2:
-                    setting(answerDB);
+                    checkSetting(management, answerDB);
                     break;
                 case 3:
-                    transfer(answerDB);
+                    checkTransfer(management, answerDB);
                     break;
                 case 4:
-                    report(answerDB);
+                    checkReport(management, answerDB);
                     break;
                 case 99:
                     break;
@@ -141,5 +143,41 @@ public class BranchMenu extends MainMenu {
             }
         }
         return false;
+    }
+
+    private void checkContact(Management management, AnswerRequestDatabase answerDB) {
+        Permission permission = management.getPermission();
+        if (!permission.isContact()) {
+            System.out.println(Constant.RED + "you do not have permission");
+            return;
+        }
+        contact(answerDB);
+    }
+
+    private void checkSetting(Management management, AnswerRequestDatabase answerDB) {
+        Permission permission = management.getPermission();
+        if (!permission.isSetting()) {
+            System.out.println(Constant.RED + "you do not have permission");
+            return;
+        }
+        setting(answerDB);
+    }
+
+    private void checkTransfer(Management management, AnswerRequestDatabase answerDB) {
+        Permission permission = management.getPermission();
+        if (!permission.isTransfer()) {
+            System.out.println(Constant.RED + "you do not have permission");
+            return;
+        }
+        transfer(answerDB);
+    }
+
+    private void checkReport(Management management, AnswerRequestDatabase answerDB) {
+        Permission permission = management.getPermission();
+        if (!permission.isRequest()) {
+            System.out.println(Constant.RED + "you do not have permission");
+            return;
+        }
+        report(answerDB);
     }
 }
